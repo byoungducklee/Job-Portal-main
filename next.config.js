11d48e045d886f7cbb88554360698ae31f79bca8
bdlee <bdlee113@ordocit.com>
2023-06-01 10:14:38 +0900

    cors적용3차

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import NextCors from 'nextjs-cors';
 
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -20,21 +19,6 @@ module.exports = {
     ]
   }
 };
-
-
-// async function handler(req, res) {
-//    // Run the cors middleware
-//    // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-//    await NextCors(req, res, {
-//       // Options
-//       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//       origin: '*',
-//       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-//    });
-
-//    // Rest of the API logic
-//    res.json({ message: 'Hello NextJs Cors!' });
-// }
 
 
 const nextConfig = {
